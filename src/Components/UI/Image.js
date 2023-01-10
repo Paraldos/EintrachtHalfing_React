3bb71825ae709013c8811af1c8d757854e1150ad
Lil Cthulhu <54956252+Paraldos@users.noreply.github.com>
2023-01-10 16:47:28 +0100

    working on lazy loading images

--- src/Components/UI/Image.js
+++ src/Components/UI/Image.js
@@ -4,7 +4,7 @@ import styles from "./Image.module.css";
 export default function ({ image, description, alt = "" }) {
   return (
     <div className={styles.image}>
-      <img src={image} alt={alt != "" ? alt : description}></img>
+      <img src={image} alt={alt != "" ? alt : description} loading="lazy"></img>
       {description != "" && <p>{description}</p>}
     </div>
   );
